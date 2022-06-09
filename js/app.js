f3f8a5d535d1e98f381228065bd59c7af53f064c
Jibon <3djibon1@gmail.com>
2022-06-10 00:35:30 +0600

    v-1.1 done

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+
+    $("#wave").wavify({
+        height: 100,
+        bones: 4,
+        amplitude: 60,
+        color: "#80ff80",
+        speed: 0.25,
+    });
+    
+})(jQuery);
